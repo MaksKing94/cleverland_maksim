a0431514dd439732df6753840b74b5b5ae81d50a
Maksim Karaliou <max.korolev@gmail.com>
2020-06-13 15:27:33 +0300

    master

---
+++ 13.06.20/script.js
@@ -0,0 +1,23 @@
+let a=12.5;
+alert (typeof(a));
+
+let b=245;
+alert (typeof(b));
+
+let c="Hello";
+alert (typeof(c));
+
+let d=undefined;
+alert (typeof(d));
+
+let e=null;
+alert (typeof(e));
+
+let f=true;
+alert (typeof(f));
+
+let g=[1,5,7,12,3,'значение'];
+alert (typeof(g));
+
+let h={'Вася':'Имя'};
+alert (typeof(h));
