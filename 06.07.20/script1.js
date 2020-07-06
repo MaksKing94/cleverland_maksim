2570a8a4257da04bdde6c6a9534a4f13b953cc47
Maksim Karaliou <max.korolev@gmail.com>
2020-07-06 20:13:31 +0300

    Object

---
+++ 06.07.20/script1.js
@@ -0,0 +1,26 @@
+'use strict'
+let person = {
+    name: "Maksim",
+    surname: "Korolev",
+    'middle name': "Sergei",
+    'date of birth': '27.02.1994',
+    profession: "conductor",
+    experience: 2,
+}
+delete person['middle name'];
+if('middle name' in person){
+    alert (person['middle name']);
+} else { 
+    alert("Такого свойства нет"); 
+}
+
+let person2 = {}
+person2.name = person.name;
+person2.surname = person.surname;
+person2['date of birth'] = person['date of birth'];
+
+let count=0;
+for (let key in person){
+    count++;
+}
+alert(count);
